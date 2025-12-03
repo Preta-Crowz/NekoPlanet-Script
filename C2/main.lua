c50e30178e1e41b440ab506099e5d333f4573a37
syeyoung <cyoung06@naver.com>
2025-12-04 05:18:04 +0900

    Just started talking with server

--- C2/main.lua
+++ C2/main.lua
@@ -1,17 +1,17 @@
 local tcp = component.proxy(component.list("internet")())
-local cid = component.proxy(component.list("eeprom")()).getData()
+local eepromdata = load(component.proxy(component.list("eeprom")()).getData())()
 
 local function connect()
   while true do
-    log("Conn...")
-    local sock = tcp.connect("115.139.146.62", 23587)
+    print("Conn...")
+    local sock = tcp.connect(eepromdata.ip, eepromdata.port)
     while true do
-      local status, message = tcp.finishConnect()
+      local status, message = sock.finishConnect()
       if status then
         return sock
       end
       if message ~= nil then
-        log(message)
+        print(message)
         os.sleep(5)
         break
       end
@@ -21,7 +21,8 @@ end
 
 local function send(sock, t)
   local data = serialization.serialize(t, false) .. "\n"
-  sock.write(data)
+  local prefix = string.format("%05d", #data)
+  sock.write(prefix .. data)
 end
 
 local function listen(sock, commands)
@@ -29,27 +30,30 @@ local function listen(sock, commands)
   while true do
     local chunk, msg = sock.read()
     if chunk == nil then
-      log(chunk)
+      print(chunk)
       return false
     end
     buffer = buffer .. chunk
-    local nl = buffer:find("\n")
-    while nl do
-      local line = buffer:sub(1, nl - 1)
-      buffer = buffer:sub(nl + 1)
-      local ok2, msg = pcall(serialization.unserialize, line)
+    while #buffer >= 5 do
+      local len = tonumber(buffer:sub(1,5))
+      if #buffer < 5 + len then break end
+      
+      local payload = buffer:sub(6, 5+len)
+      buffer = buffer:sub(6+len)
+      
+      local ok2, msg = pcall(serialization.unserialize, payload)
       if ok2 and type(msg) == "table" and msg.cmd then
+        print(msg.cmd)
         local res = commands[msg.cmd](table.unpack(msg.args))
         send(sock, res)
       end
-      nl = buffer:find("\n")
     end 
   end
 end
 
 actions = {}
 function actions.identify()
-  return {cid=cid}
+  return eepromdata
 end
 
 function actions.load(payload)
@@ -58,5 +62,5 @@ end
 
 while true do
   local sock = connect()
-  handleCommands(sock, actions)
+  listen(sock, actions)
 end
