36d6102295743542a7f03d5e86e47132b2de87d1
Preta-Crowz <preta@siro.dev>
2026-02-19 21:02:32 +0900

    add certus.lua

---
+++ CC/certus.lua
@@ -0,0 +1,35 @@
+local drawer = peripheral.find("functionalstorage:storage_controller")
+local reactor = peripheral.find("advanced_ae:reaction_chamber")
+
+local charged = "ae2:charged_certus_quartz_crystal"
+locat dust = "ae2:certus_quartz_dust"
+
+local function findIndex(id)
+  for k, v in pairs(drawer.list()) do
+    if v.name == id then return k end
+  end
+  return -1
+end
+
+local function getReactorCount(id)
+  local count = 0
+  for _, v in pairs(reactor.list()) do
+    if v.name == id then
+      count = count + v.count
+    end
+  end
+  return count
+end
+
+local next = charged
+
+local function loop()
+  local count = getReactorCount(next)
+  if count <= 64 then
+    drawer.pushItems(peripheral.getName(reactor), findIndex(next), 256 - count)
+  end
+  if next == charged then next = dust else next = charged end
+  os.sleep(0.05)
+end
+
+parallel.waitForAll(loop)
